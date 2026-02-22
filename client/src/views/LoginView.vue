ad9d6f7ecfd0884d95d7e2b143f6e957c2ea8799
timmytay <1trofim.kraves@gmail.com>
2026-02-22 22:08:27 +0800

    c32m24ga: надо оптимизировать код

--- client/src/views/LoginView.vue
+++ client/src/views/LoginView.vue
@@ -13,31 +13,28 @@ const router = useRouter();
 async function clickForm() {
   errorMessage.value = '';
   isLoading.value = true;
-  
-  try {
-    const success = await authStore.login({
-      username: username.value,
-      password: password.value,
-    });
-    
-    if (success) {
-      await router.push({ name: 'ProjectsView' });
-    } else {
-      errorMessage.value = 'Неверное имя пользователя или пароль';
-    }
-  } catch (error) {
-    errorMessage.value = 'Ошибка при входе. Попробуйте еще раз.';
-  } finally {
-    isLoading.value = false;
-    username.value = '';
-    password.value = '';
+
+  const success = await authStore.login({
+    username: username.value,
+    password: password.value,
+  });
+
+  if (success) {
+    await router.push({ name: 'ProjectsView' });
+  } else {
+    errorMessage.value = 'Неверное имя пользователя или пароль';
   }
+  isLoading.value = false;
+  username.value = '';
+  password.value = '';
+
 }
 </script>
 
 <template>
   <form @submit.stop.prevent="clickForm" class="form d-flex flex-column p-3" style="gap: 8px">
-    <input placeholder="Логин (пример: tay)" class="form-control" type="text" v-model="username" :disabled="isLoading" required>
+    <input placeholder="Логин (пример: tay)" class="form-control" type="text" v-model="username" :disabled="isLoading"
+      required>
     <input placeholder="Пароль" class="form-control" type="password" v-model="password" :disabled="isLoading" required>
     <button class="btn btn-info" :disabled="isLoading">
       <span v-if="isLoading">Вход...</span>
@@ -48,5 +45,4 @@ async function clickForm() {
     </div>
   </form>
 </template>
-<style>
-</style>
+<style></style>
