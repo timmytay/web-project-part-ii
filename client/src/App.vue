5fc4e9e1066ac47ed140b73da63a4b4d7e613760
timmytay <1trofim.kraves@gmail.com>
2026-03-03 21:40:41 +0800

    c32m32bb: фикс аппа. осталось повторить, кто что делает :)

--- client/src/App.vue
+++ client/src/App.vue
@@ -19,44 +19,61 @@ async function onLogout() {
 </script>
 
 <template>
-  <div class="container">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <a class="navbar-brand" href="/">менеджер проектов</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
-        aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+  <div class="container mt-3">
+    <nav class="navbar navbar-expand-lg navbar-light bg-white shadow-sm rounded-3 px-3 py-2">
+      <router-link class="navbar-brand fw-semibold text-primary" to="/">
+        <i class="bi bi-book me-2"></i> Менеджер проектов
+      </router-link>
+
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown">
         <span class="navbar-toggler-icon"></span>
       </button>
+
       <div class="collapse navbar-collapse" id="navbarNavDropdown">
-        <ul class="navbar-nav me-auto">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+
           <li class="nav-item">
-            <router-link class="nav-link" to="/">Проекты</router-link>
+            <router-link class="nav-link px-3" to="/">Проекты</router-link>
           </li>
+
           <li class="nav-item">
-            <router-link class="nav-link" to="/columns">Колонки</router-link>
+            <router-link class="nav-link px-3" to="/columns">Колонки</router-link>
           </li>
+
           <li class="nav-item">
-            <router-link class="nav-link" to="/tasks">Задачи</router-link>
+            <router-link class="nav-link px-3" to="/tasks">Задачи</router-link>
           </li>
+
           <li class="nav-item">
-            <router-link class="nav-link" to="/comments">Комментарии</router-link>
+            <router-link class="nav-link px-3" to="/comments">Комментарии</router-link>
           </li>
+
           <li class="nav-item">
-            <router-link class="nav-link" to="/time-tracking">Учет времени</router-link>
+            <router-link class="nav-link px-3" to="/time-tracking">Учет времени</router-link>
           </li>
+
           <li v-if="is_authenticated" class="nav-item">
-            <router-link class="nav-link" to="/second-auth">Прохождение 2FA</router-link>
+            <router-link class="nav-link px-3" to="/second-auth">2FA</router-link>
           </li>
+
           <li v-if="is_staff" class="nav-item">
-            <router-link class="nav-link" to="/users">Пользователи</router-link>
+            <router-link class="nav-link px-3" to="/users">Пользователи</router-link>
           </li>
         </ul>
-        <div class="d-flex align-items-center gap-2">
-          <router-link v-if="is_authenticated" to="/users" class="btn btn-outline-primary btn-sm">
-            <i class="bi bi-person-circle me-1"></i> {{ username }}
-            <span v-if="is_staff">(админ)</span>
+
+        <div v-if="is_authenticated" class="d-flex align-items-center gap-2">
+
+          <router-link to="/users" class="btn btn-outline-primary btn-sm">
+            <i class="bi bi-person-circle me-1"></i>
+            {{ username }}
+            <span v-if="is_staff"> (админ)</span>
           </router-link>
-          <button v-if="is_authenticated" @click="onLogout" class="btn btn-outline-danger btn-sm">
-            <i class="bi bi-box-arrow-right"></i> Выйти</button>
+
+          <button @click="onLogout" class="btn btn-outline-danger btn-sm">
+            <i class="bi bi-box-arrow-right me-1"></i>
+            Выйти
+          </button>
+
         </div>
       </div>
     </nav>
